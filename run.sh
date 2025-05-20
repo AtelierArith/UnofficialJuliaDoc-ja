bcc015b91e0f21d13ce483026ec65ff2a52f0af3
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2025-05-20 13:28:56 +0900

    assets と run.sh を追加

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -eux
+
+cp assets/Project_1.11.5.toml julia/doc/Project.toml
+cp assets/Manifest_1.11.5.toml julia/doc/Manifest.toml
+cp assets/make_1.11.5.jl julia/doc/make.jl
+
+make -C julia
+make -C julia/doc
