431c7da31bacc474d167b6db5cc99b5d0e9c6a36
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2025-10-19 10:28:59 +0900

    Support 1.12.1

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -eux
 
-cp assets/Project_1.11.5.toml julia/doc/Project.toml
-cp assets/Manifest_1.11.5.toml julia/doc/Manifest.toml
-cp assets/make_1.11.5.jl julia/doc/make.jl
+cp assets/Project_1.12.1.toml julia/doc/Project.toml
+cp assets/Manifest_1.12.1.toml julia/doc/Manifest.toml
+cp assets/make_1.12.1.jl julia/doc/make.jl
 
 make -C julia
 make -C julia/doc
